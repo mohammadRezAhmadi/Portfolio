5cf3e8deaaf9e4fa3dff4dcb18dbf37101547e55
mhmrez <mohammadahmadicoder@gmail.com>
2024-12-28 10:49:00 +0330

    add new skill

--- src/Pages/skills/dataSkills.js
+++ src/Pages/skills/dataSkills.js
@@ -3,11 +3,12 @@ const dataSkills = [
     {id:2 , img:'/css.png' , title:'Css / Scss' , level:'Dominant'},
     {id:3 , img:'/js.png' , title:'JS (ES6+)' , level:'Dominant'},
     {id:4 , img:'/bootstrap.svg' , title:'Boostrap 5' , level:'Dominant'},
-    {id:5 , img:'/jquery.svg' , title:'Jquery' , level:'Dominant'},
-    {id:6 , img:'/material.svg' , title:'Material Ui' , level:'Dominant'},
-    {id:7 , img:'/npm.svg' , title:'Npm' , level:'Dominant'},
-    {id:8 , img:'/react.png' , title:'ReactJS' , level:'Juinior'},
-    {id:9 , img:'/github.svg' , title:'Github' , level:'Dominant'},
-    {id:10 , img:'/api.svg' , title:'Rest Api' , level:'Dominant'},
+    {id:5 , img:'/tailwind-css.svg' , title:'Tailwind CSS' , level:'Dominant'},
+    {id:6 , img:'/jquery.svg' , title:'Jquery' , level:'Dominant'},
+    {id:7 , img:'/material.svg' , title:'Material Ui' , level:'Dominant'},
+    {id:8 , img:'/npm.svg' , title:'Npm' , level:'Dominant'},
+    {id:9 , img:'/react.png' , title:'ReactJS' , level:'Juinior'},
+    {id:10 , img:'/github.svg' , title:'Github' , level:'Dominant'},
+    {id:11 , img:'/api.svg' , title:'Rest Api' , level:'Dominant'},
 ]
 export default dataSkills
